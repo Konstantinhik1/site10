e91520b57c329ad42981595d727bcc2f5603bffe
kostya <kuharchikkostua@gmail.com>
2024-11-29 07:15:42 -0800

    continuation of work

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,3 +1,59 @@
 from django.contrib import admin
+from django.contrib.gis.gdal.field import Field
 
-# Register your models here.
+from .models import Product, Order
+
+class OrderInline(admin.TabularInline):
+    model = Product.orders.through
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        OrderInline,
+    ]
+    list_display = "pk","name", "description_short", "price", "discount"  #"archived"
+    list_display_links = "pk", "name"
+    ordering ="-name", "pk"
+    search_fields = "name", "description"
+    fieldsets = [
+        (None, {
+            "fields": ("name", "description"),
+        }),
+        ("Price options", {
+            "fields": ("price", "discount"),
+            "classes": ("collapse", "wide"),
+        })
+        # ("Extra options",{
+        #     "fields": ("archived",),
+        #     "classes": ("colapse",),
+        #     "description": "Extra options.Field 'archived' is for soft delete",
+        # })
+
+    ]
+
+    def description_short(self,obj: Product) -> str:
+        if len(obj.description) < 48:
+            return obj.description
+        return obj.description[:48] + "..."
+
+# admin.site.register(Product, ProductAdmin)
+
+# class ProductInline(admin.TabularInline):
+class ProductInline(admin.StackedInline):
+    model = Order.products.through
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductInline,
+    ]
+    list_display = "delivery_address", "promocode", "created_at", "user"
+
+    def get_queryset(self, request):
+        return Order.objects.select_related("user").prefetch_related("products")
+
+    def user_verbose(self,obj: Order) -> str:
+        return obj.user.first_name or obj.user.username
+
+
--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+
 class Product(models.Model):
     class Meta:
         ordering = ["name", "price"]
@@ -14,9 +16,20 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     arhived = models.BooleanField(default=False)
 
+    # @property
+    # def description_short(self) -> str:
+    #     if len(self.description) < 48:
+    #         return self.description
+    #     return self.description[:48] + "..."
+
+    def __str__(self) -> str:
+        return f"Product(pk={self.pk}, name={self.name!r})"
+
 class Order(models.Model):
     delivery_address = models.TextField( null=True, blank=True)
     promocode = models.CharField(max_length=20, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     products = models.ManyToManyField(Product, related_name="orders")
+
+
