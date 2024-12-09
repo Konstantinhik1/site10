6b1a814b097f8d9d73149eaf9ca4fccbfb52b620
kostya <kuharchikkostua@gmail.com>
2024-12-09 03:47:44 -0800

    POST_request

--- mysite/requestdataapp/urls.py
+++ mysite/requestdataapp/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 
-from .views import process_get_view , user_form
+from .views import process_get_view , user_form, handle_file_upload
 
 app_name = "requestdataapp"
 
 urlpatterns = [
     path("get/", process_get_view,name="get-vew"),
     path("bio/", user_form, name="user-form"),
+    path("upload/",handle_file_upload, name="file_upload"),
 
 ]
 
