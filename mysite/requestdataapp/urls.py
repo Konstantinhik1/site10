5dbf664d87c5cd7f8bf39fedc33c9f024214f13a
kostya <kuharchikkostua@gmail.com>
2024-12-09 01:58:06 -0800

    work request-query-params.html

--- mysite/requestdataapp/urls.py
+++ mysite/requestdataapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import process_get_view
 
-app_name = "shopapp"
+app_name = "requestdataapp"
 
 urlpatterns = [
     path("get/", process_get_view,name="get-vew")
--- mysite/requestdataapp/views.py
+++ mysite/requestdataapp/views.py
@@ -2,7 +2,13 @@ from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
 def process_get_view(request: HttpRequest) -> HttpResponse:
+    a = request.GET.get("a","")
+    b = request.GET.get("b", "")
+    result = a + b
     context = {
+        "a" : a,
+        "b" : b,
+        "result": result,
 
     }
     return render(request, "requestdataapp/request-query-params.html",context=context)
