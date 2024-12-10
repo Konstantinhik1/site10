7582eae728f26a1449c87bfe8ec73334f113b3eb
kostya <kuharchikkostua@gmail.com>
2024-12-10 01:58:08 -0800

    GroupsVieW

--- mysite/shopapp/forms.py
+++ mysite/shopapp/forms.py
@@ -1,7 +1,18 @@
 from django import forms
 from .models import Product
+from django. forms import ModelForm
+from django.contrib.auth.models import Group
+
+
+
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = "name", "price", "description", "discount"
+
+
+class GroupForm(ModelForm):
+    class Meta:
+        model = Group
+        fields = ["name"]
--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,36 +1,57 @@
 from timeit import default_timer
+
+from django.contrib.admindocs.views import ViewIndexView
 from django.contrib.auth.models import Group
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render, redirect, reverse
-from .forms import ProductForm
-from .forms import ProductForm
+from django.views import View
+
+from .forms import ProductForm , GroupForm
 from .models import Product, Order
 
-def shop_index(request: HttpRequest):
-    products = [
-        ('Laptop', 1999),
-        ('Desktop', 2999),
-        ('Smartphone', 999),
-    ]
-    context = {
-        "time_running": default_timer(),
-        "products": products,
-    }
-    return render(request, 'shopapp/shop-index.html', context=context)
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render
+from django.views import View
+from timeit import default_timer
 
-def groups_list(request: HttpRequest):
-    context = {
-        "groups": Group.objects.prefetch_related('permissions').all(),
-    }
-    return render(request, 'shopapp/groups-list.html', context=context)
+class ShopIndexView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        products = [
+            ('Laptop', 1999),
+            ('Desktop', 2999),
+            ('Smartphone', 999),
+        ]
+        context = {
+            "time_running": default_timer(),
+            "products": products,
+        }
+        return render(request, 'shopapp/shop-index.html', context=context)
+
+class GroupsListView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        context = {
+            "form": GroupForm(),
+            "groups": Group.objects.prefetch_related('permissions').all(),
+        }
+        return render(request, 'shopapp/groups-list.html', context=context)
+
+    def post(self, request:HttpRequest):
+        form = GroupForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect(request.path)
+
 
 def products_list(request: HttpRequest):
     context = {
         "products": Product.objects.all(),  # Исправлена опечатка "prodducts"
     }
     return render(request, 'shopapp/products-list.html', context=context)
+
+
 
  # Ensure this import is correct and the form exists
 
