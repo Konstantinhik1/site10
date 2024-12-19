7bb8543a7a12ebb45553aefc07c7f52bc917d60c
kostya <kuharchikkostua@gmail.com>
2024-12-19 01:40:30 -0800

    Based Views

--- mysite/myapiapp/views.py
+++ mysite/myapiapp/views.py
@@ -1,8 +1,20 @@
+from django.contrib.auth.handlers.modwsgi import groups_for_user
+from django.contrib.auth.models import Group
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
+from rest_framework.views import APIView
+from sqlparse.engine.grouping import group
+
 
 @api_view()
 def hello_world_view(request: Request) ->Response:
     return Response({"messege": "Hello World!"})
+
+
+class GroupsListView(APIView):
+    def get(self, request: Request) -> Response:
+        groups =Group.objects.all()
+        data = [group.name for group in groups]
+        return Response({"groups": data})
 
