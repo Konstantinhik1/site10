ee7eb780a332059c5bdc7130a21ad3feb364e1a0
kostya <kuharchikkostua@gmail.com>
2024-12-19 06:25:51 -0800

    REST Fr

--- mysite/myapiapp/views.py
+++ mysite/myapiapp/views.py
@@ -1,20 +1,21 @@
-from django.contrib.auth.handlers.modwsgi import groups_for_user
 from django.contrib.auth.models import Group
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.views import APIView
-from sqlparse.engine.grouping import group
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.permissions import IsAuthenticated
 from .serializer import GroupSerializer
 
+
 @api_view()
-def hello_world_view(request: Request) ->Response:
-    return Response({"messege": "Hello World!"})
+def hello_world_view(request: Request) -> Response:
+    return Response({"message": "Hello World!"})  # Исправлено: "messege" → "message"
 
 
-class GroupsListView(APIView):
-    def get(self, request: Request) -> Response:
-        groups =Group.objects.all()
-        serialized = GroupSerializer(groups, many=True)
-        return Response({"groups": serialized.data})
+class GroupsListView(ListCreateAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [IsAuthenticated]  # Ограничен доступ только для аутентифицированных пользователей
+
+
 
